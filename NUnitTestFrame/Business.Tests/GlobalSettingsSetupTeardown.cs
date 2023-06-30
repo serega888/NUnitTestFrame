cad9f070ff7dbdafaaf850c047b54f32f21d5e61
Sergey_Fadin <sergey.fadin@outlook.com>
2023-06-30 15:03:45 +0300

    Global settings for Namespace or Assembly

---
+++ NUnitTestFrame/Business.Tests/GlobalSettingsSetupTeardown.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+[SetUpFixture]
+class AssemblySetUpTearDown
+{
+	[OneTimeSetUp]
+	public void AssemblySetUp()
+	{
+		Trace.WriteLine("Assembly setup");
+	}
+
+	[OneTimeTearDown]
+	public void AssemblyTearDown()
+	{
+		Trace.WriteLine("Assembly teardown");
+	}
+}
+
+namespace Business.Tests
+{
+	[SetUpFixture]
+	class NameSpaceSetUpTearDown
+	{
+		[OneTimeSetUp]
+		public void NameSpaceSetUp()
+		{
+			Trace.WriteLine("NameSpace setup");
+		}
+
+		[OneTimeTearDown]
+		public void NameSpaceTearDown()
+		{
+			Trace.WriteLine("NameSpace teardown");
+		}
+	}
+
+}

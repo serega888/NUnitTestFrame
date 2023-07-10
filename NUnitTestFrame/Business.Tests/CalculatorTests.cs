120925df7b2ff28e54a1e87c5b08f8bdfb865a38
Sergey_Fadin <sergey.fadin@outlook.com>
2023-07-11 00:00:20 +0300

    Саlculator test with TDD(Red->Green)

---
+++ NUnitTestFrame/Business.Tests/CalculatorTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Tests
+{
+	[TestFixture]
+	public class CalculatorTests
+	{
+		[Test]
+		[TestCase(5, 4, 9)]
+		public void AddNumbers_ValidValues_ReturnsCorrectResult(int a, int b, int c)
+		{
+			var calculator = new Calculator(a, b);
+			int result = calculator.AddNumbers();
+
+			Assert.That(result, Is.EqualTo(c));
+		}
+
+	}
+}
---
+++ NUnitTestFrame/Business/Calculator.cs
@@ -0,0 +1,16 @@
+﻿namespace Business
+{
+	public class Calculator
+	{
+		private int a, b;
+		public Calculator(int a, int b)
+		{
+			this.a = a;
+			this.b = b;
+		}
+		public int AddNumbers()
+		{
+			return a + b;
+		}
+	}
+}

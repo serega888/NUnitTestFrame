83a426b5075d02898cd8c32f09b1df0a963a4903
Sergey_Fadin <sergey.fadin@outlook.com>
2023-07-10 19:44:18 +0300

    tests with NSubstitute for Device

---
+++ NUnitTestFrame/Business.Tests/DeviceTests.cs
@@ -0,0 +1,41 @@
+﻿using NSubstitute;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Tests
+{
+	[TestFixture]
+	public class DeviceTests
+	{
+		[Test]
+		public void Connect_FailedThrice_ThreeTries()
+		{
+			IProtocol provider = Substitute.For<IProtocol>();
+			provider.Connect(Arg.Any<string>()).ReturnsForAnyArgs(false);
+
+			var device = new Device(provider);
+			device.Connect(String.Empty);
+
+			provider.Received(3).Connect(Arg.Any<string>());
+		}
+
+		[Test]
+		public void Find_FoundCOM8_ReturnsCOM8()
+		{
+			IProtocol provider = Substitute.For<IProtocol>();
+			var device = new Device(provider);
+			Task<string> task = device.Find();
+
+			const string portName = "COM8";
+
+			provider.SearchingFinished += Raise.Event<EventHandler<DeviceSearchingEventArgs>>(provider, new DeviceSearchingEventArgs(portName));
+
+			Assert.That(task.Result, Is.EqualTo(portName));
+		}
+
+	}
+}
